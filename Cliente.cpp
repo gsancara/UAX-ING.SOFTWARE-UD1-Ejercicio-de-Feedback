386c0ac8d6d26995b08b668251c559c7483b94be
gsancara <46517319+gsancara@users.noreply.github.com>
2019-01-09 22:16:28 +0100

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,36 +1,51 @@
-#include <string.h>
-#include <vector>
-#include "Coche.h"
-class Cliente
-{
-private:
-std::string _name;
-std::vector< Coche* > _cars;
-unsigned int _money;
-public:
-Cliente( std::string name );
-// getters & setters
-std::string getName();
-void addMoney( unsigned int amount );
-void expendMoney( unsigned int amount );
-unsigned int getMoneyAmount();
-/*!
-\brief Añade un nuevo coche
-@returns true si se añade correctamente, false si el coche ya
-estaba añadido
-*/
-bool addCar( Coche* car );
-/*!
-\brief Elimina un coche del parque del cliente
-@returns true si se Elimina correctamente, false si no estaba
-en la lista
-*/
-4
-bool removeCar( std::string license );
 /*!
 \brief Busca un coche en el parque del cliente
-@returns el coche en cuestión, NULL si no lo encuentra...
+@returns el coche en cuestión, NULL si no lo encuentra
 */
-Coche* searchCar( std::string license );
-virtual ~Cliente(void);
-};
+Coche* searchCar( std::string license )
+6
+{
+std::vector< Coche* >::iterator search_pos = _cars.begin();
+while( search_pos != _cars.end() )
+{
+if( (*search_pos)->getLicense() == license ) // encontrado!
+return &*search_pos;
+search_pos++;
+}
+return NULL;
+}
+/*!
+\brief Elimina un coche del parque del cliente
+@returns true si se Elimina correctamente, false si no estaba en la
+lista
+*/
+bool removeCar( std::string license )
+{
+Coche* borrable = NULL;
+if( ( borrable = searchCar( car->getLicense() ) ) != NULL )
+{
+// el coche está en la lista, lo borro
+std::vector< Coche* >::iterator position = std::find(
+_cars.begin(), _cars.end(), borrable );
+_cars.erase( position );
+return true;
+}
+// no se encontró el coche
+return false;
+}
+/*!
+\brief Añade un nuevo coche
+@returns true si se añade correctamente, false si el coche ya estaba
+añadido
+*/
+bool addCar( Coche* car )
+{
+if( searchCar( car->getLicense() ) == NULL )
+{
+// el coche no está en la lista, lo añado
+_cars.push_back( car );
+return true;
+}
+// el coche ya estaba en la lista
+return false;
+}
